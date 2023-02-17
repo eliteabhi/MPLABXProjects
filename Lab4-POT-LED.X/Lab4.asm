b9c04efd55702860022ce81dfdf24e28d2c772ca
eliteabhi <abhirangarajan2003@gmail.com>
2023-02-17 10:12:23 -0600

    Lab 4A

---
+++ Lab4-POT-LED.X/Lab4.asm
@@ -0,0 +1,137 @@
+;NAME: Abhi Rangarajan
+;LAB 4A POT LED in ASM
+
+; PIC16F1829 Configuration Bit Settings
+
+; Assembly source line config statements
+
+#include "p16f1829.inc"
+
+; CONFIG1
+; __config 0x9E4
+ __CONFIG _CONFIG1, _FOSC_INTOSC & _WDTE_OFF & _PWRTE_OFF & _MCLRE_ON & _CP_OFF & _CPD_OFF & _BOREN_OFF & _CLKOUTEN_OFF & _IESO_OFF & _FCMEN_OFF
+; CONFIG2
+; __config 0x3EFF
+ __CONFIG _CONFIG2, _WRT_OFF & _PLLEN_OFF & _STVREN_ON & _BORV_LO & _LVP_ON
+ 
+    list p=16F1829, R=DEC
+
+    CBLOCK 0x20 ; Define GPR variable register locations
+VarA
+VarB
+    ENDC
+  
+    ORG 0
+    goto Start   
+;!!PLACE ISR HERE!!
+    RETFIE
+    
+Start
+;Clock Setup
+    BANKSEL	OSCCON 	
+    movlw   	0x6A   	 	
+    movwf  	OSCCON
+;Input Output Setup
+    ;Configure all of TRISA to be output
+    BANKSEL 	TRISA  	 	
+    clrf    	TRISA
+    ;Set all LATCH A outputs to 0
+    BANKSEL	LATA
+    clrf	LATA
+    ;Configure Push Button as Input
+    BANKSEL	TRISB
+    bsf		TRISB, 7
+    ;Configure Push Input as Digital
+    BANKSEL	ANSELB
+    bcf		ANSELB, 7
+    ;Configure Option Registor for TMR0
+    BANKSEL	OPTION_REG
+    movlw	0x87
+    movwf	OPTION_REG
+ 
+    MOVWF CCP2CON
+    BANKSEL CCPR2L
+    CLRF  CCPR2L  ;Set initial duty cycle to 0= ?Green LED full ON?
+    BANKSEL CCPTMRS
+    CLRF  CCPTMRS ;Select timer 2 for all ccp modules
+    BANKSEL PIR2
+    BCF  PIR2, 1
+    BANKSEL T2CON 
+    MOVLW b'00000101'  ;1:1 postscaler, 4:1 prescaler and TMR2 turned on
+    MOVWF T2CON
+    
+    PWM_Initilize
+	BANKSEL APFCON1
+	BSF APFCON1, 0
+	BANKSEL TRISA
+	BSF  TRISA,5 ;This is a safety move, we will turn on later
+	BANKSEL PR2
+	MOVLW 0XFF
+	MOVWF PR2  ;This will give about 1KHz as a pulse frequency
+	BANKSEL CCP2CON ;Most of the CCP2 registers are in the same bank 
+	MOVLW 0X0C
+
+    
+    ADC_Initilize
+	Banksel  ADCON0    ;Note  ADCON0 and ADCON1 in same bank
+	MOVLW 0X2D          ;Set channel to AN11/RB5, turn on ADC 
+	MOVWF ADCON0
+	MOVLW 0X40       ;Left justified, Fosc/4 and Vss and Vdd as ref
+	MOVWF ADCON1
+	BANKSEL TRISB       ;Configure RB5 as analog input
+	BSF  TRISB, 5
+	BANKSEL ANSELB
+	BSF  ANSELB, 5
+	RETURN
+
+    ADC_Read
+	BANKSEL  ADCON0
+	BSF  ADCON0, 1 ;This starts a conversion process
+	BTFSC  ADCON0, 1
+	GOTO  $-1
+	RETURN   ;Returns when done with result in ADRESH
+	
+    PWM_Duty
+	BANKSEL ADRESH
+	MOVF  ADRESH, W  ;must put in the 2nd operand of W
+	BANKSEL CCPR2L
+	MOVWF CCPR2L
+	RETURN
+
+	
+    Call ADC_Initilize
+    Call PWM_Initilize
+    
+    Wait1
+	BANKSEL  PIR1
+	BTFSS    PIR1, 1
+	GOTO     Wait1
+	BANKSEL  TRISA
+	BCF  TRISA, 5   ;Starts the PWM working
+	
+    ;Execution Loop
+    Loop
+	call ADC_Read
+	call PWM_Duty
+	call Delay
+	goto Loop
+    
+    ;16 bit variable based delay
+Delay
+    BANKSEL VarA
+    movlw   0x20
+    movwf   VarA
+Outside
+    decfsz  VarA
+    goto    Goon
+    return      
+Goon
+    movlw   0xFF
+    movwf   VarB
+Inside
+    decfsz  VarB
+    goto    Inside
+    goto    Outside
+    
+    end
+   
---
+++ Lab4-POT-LED.X/build/default/production/Lab4.o.d
@@ -0,0 +1,3 @@
+build/default/production/Lab4.o:  \
+	/home/abhi/MPLABXProjects/Lab4-POT-LED.X/Lab4.asm  \
+
