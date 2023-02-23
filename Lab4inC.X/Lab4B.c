0ad6c167400d384e69664673af41c68567bb42a6
eliteabhi <abhirangarajan2003@gmail.com>
2023-02-22 20:13:46 -0600

    Made lab 4b

---
+++ Lab4inC.X/Lab4B.c
@@ -0,0 +1,54 @@
+
+// PIC16F1829 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG1
+#pragma config FOSC = INTOSC    // Oscillator Selection (INTOSC oscillator: I/O function on CLKIN pin)
+#pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
+#pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Memory Code Protection (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable (Brown-out Reset disabled)
+#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+#pragma config IESO = OFF       // Internal/External Switchover (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is disabled)
+
+// CONFIG2
+#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
+#pragma config PLLEN = OFF      // PLL Enable (4x PLL disabled)
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
+#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+#pragma config LVP = ON         // Low-Voltage Programming Enable (Low-voltage programming enabled)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define _XTAL_FREQ 4000000.0 /*for 4mhz Processor clock*/
+
+void main(void) {
+    
+    OSCCON = 0x6A;
+    
+    TRISA = 0x00;
+    PORTA = 0x00;
+    
+    TRISB = 0x40;
+    PORTB = 0x40;
+    
+    while (1) {
+        
+        RA5 = 1;
+        __delay_ms(500);
+        RA5 = 0;
+        __delay_ms(500);
+        
+    }
+    
+    return;
+}
---
+++ Lab4inC.X/build/default/production/Lab4B.p1.d
@@ -0,0 +1,3 @@
+ build/default/production/Lab4B.d  \
+ build/default/production/Lab4B.p1:  \
+ Lab4B.c 
